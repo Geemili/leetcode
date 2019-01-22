066055d45ad1ea24c4bcdb3f638141451d0f0000
LeRoyce Pearson <leroycepearson@geemili.xyz>
2019-01-21 20:51:42 -0600

    Add template solution file

---
+++ template.rs
@@ -0,0 +1,17 @@
+// Add solution here
+
+
+#[cfg(test)]
+use test::*;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+    }
+
+    // Definitions
+    pub struct Solution;
+}
